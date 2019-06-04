105778a59a3fd06d6520ab9721dfe456abbdea91
Igor <igor.artzona@gmail.com>
2019-06-04 17:49:33 +0300

    Update from plugin handbook

--- wp-quick-plugin.php
+++ wp-quick-plugin.php
@@ -2,22 +2,15 @@
 /**
  * WP quick plugin
  *
- * @package   WP_Quick_Plugin
- * @author    jvj <igor.artzona@gmail.com>
- * @license   GPL-2.0+
- * @link      https://github.com/igorartzona/wp-quick-plugin
- * @copyright 2019 jvj, artzona.org
- *
- * @wordpress-plugin
  * Plugin Name:       WP quick plugin
  * Plugin URI:        https://github.com/igorartzona/wp-quick-plugin
- * Description:       Заготовка для быстрого плагина WP
+ * Description:       Содержит только файл с необходимыми тэгами. Скачал -> установил -> можно говнокодить
  * Version:           0.1.0
  * Author:            jvj
- * Author URI:        https://github.com/igorartzona
- * Text Domain:       wp-quick-plugin
+ * Author URI:        https://github.com/igorartzona * 
  * License:           GPL-2.0+
  * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Text Domain:       wp-quick-plugin
  * Domain Path:       /languages
  */
 // If this file is called directly, abort.
