5b2559297cd58e4ec8271443b7c20a70eae10759
Igor <igor.artzona@gmail.com>
2019-06-04 17:33:55 +0300

    Create wp-quick-plugin.php

---
+++ wp-quick-plugin.php
@@ -0,0 +1,26 @@
+<?php
+/**
+ * WP quick plugin
+ *
+ * @package   WP_Quick_Plugin
+ * @author    jvj <igor.artzona@gmail.com>
+ * @license   GPL-2.0+
+ * @link      https://github.com/igorartzona/wp-quick-plugin
+ * @copyright 2019 jvj, artzona.org
+ *
+ * @wordpress-plugin
+ * Plugin Name:       WP quick plugin
+ * Plugin URI:        https://github.com/igorartzona/wp-quick-plugin
+ * Description:       Заготовка для быстрого плагина WP
+ * Version:           0.1.0
+ * Author:            jvj
+ * Author URI:        https://github.com/igorartzona
+ * Text Domain:       wp-quick-plugin
+ * License:           GPL-2.0+
+ * License URI:       http://www.gnu.org/licenses/gpl-2.0.txt
+ * Domain Path:       /languages
+ */
+// If this file is called directly, abort.
+if ( ! defined( 'WPINC' ) ) {
+	die;
+}
